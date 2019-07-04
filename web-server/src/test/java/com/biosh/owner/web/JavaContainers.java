54816ac300870830d41da2a95fe92bf225adec92
justk <993784217@qq.com>
2019-07-04 23:29:53 +0800

    javaContainer

---
+++ web-server/src/test/java/com/biosh/owner/web/JavaContainers.java
@@ -0,0 +1,23 @@
+package com.biosh.owner.web;
+
+public class JavaContainers {
+
+  /**
+   * 1、Java容器有list、set、map
+   */
+
+  /**
+   * 2、collection和collections的区别
+   * collection是一个集合接口，是list、set的超类
+   * collections是集合工具类，里面包含了许多集合的方法,常用的有排序和反转
+   */
+
+  /**
+   *  list是列表，set是集合，map是键值映射，
+   *  其中list有arraylist和linkedList，前者实现方式是数组，后者是链表
+   *  set有hashset和treeSet,都是基于map实现的
+   *  map有hashmap和hashTable，后者是前者的安全版本，底层是数组+链表，当发生hash碰撞的时候，具有相同hash值得元素存在一条链表上	
+   */
+
+
+}
